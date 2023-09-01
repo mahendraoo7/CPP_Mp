6e0e6de27bb54bfb6478b1bc5b44ab27410838c3
mahendra purohit <mahendrapurohit9889@gmail.com>
2023-08-31 21:18:51 -0700

    01-09-23

---
+++ all_contr.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+  using namespace std;
+class square
+{
+
+   int a=5;
+
+   public:
+
+square()
+{
+    cout<<"square default contructor is call -->";
+}
+
+square(double redius)
+{
+    double area;
+    
+    area=3.14*redius*redius;
+    cout<<"area of circle is = :"<<area;;
+
+}
+square(double b ,double h)
+{
+    float area;
+    
+    area = 0.5*b*h;
+    cout<<"\nArea of triangle is = "<<area;
+    cout<<endl;
+
+
+}
+square(int len,double bre)
+{
+  float area;
+    
+    area = len*bre;
+    cout<<"\nArea of rectangle is = "<<area;
+    cout<<endl;
+
+}
+square(int len)
+{
+  float area;
+    
+    area = len*len;
+    cout<<"\nArea of square is= "<<area;
+    cout<<endl;  
+
+}
+};  
+
+int main ()
+{
+
+  square(83);
+    
+}
